a01f7eaefd6eb1313a609a7ead43903e577aa701
Nguyen Duc Thuan <nguyenducthuan24@gmail.com>
2025-04-06 08:56:49 +0700

    add model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that you can use
+  WidgetsFlutterBinding.ensureInitialized();
+  String chatJson = await rootBundle.loadString('assets/newchat.json');
+  String todo = await rootBundle.loadString('assets/todo.json');
+  List<Chat> chats = [];
+  List<Todo> todos = [];
+  (jsonDecode(chatJson) as List).map((e) {
+    Chat chat = Chat.fromJson(e);
+    chats.add(chat);
+  });
+  (jsonDecode(todo) as List).map((e) {
+    Todo todo = Todo.fromJson(e);
+    todos.add(todo);
+  });
+  print(chats);
+  print(todos);
   runApp(const MyApp());
 }
 
@@ -18,6 +37,41 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         body: Text("hello world"),
       ),
+    );
+  }
+}
+
+class Chat {
+  String name;
+  String avatar;
+  bool isRead;
+
+  Chat({
+    required this.name,
+    required this.avatar,
+    required this.isRead,
+  });
+  factory Chat.fromJson(Map<String, dynamic> json) {
+    return Chat(
+      name: json['name'],
+      avatar: json['avatar'],
+      isRead: json['isRead'],
+    );
+  }
+}
+
+class Todo {
+  String name;
+  bool isDone;
+
+  Todo({
+    required this.name,
+    required this.isDone,
+  });
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      name: json['name'],
+      isDone: json['isDone'],
     );
   }
 }
