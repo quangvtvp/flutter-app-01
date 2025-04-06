eb57ebabf5c8006bab3bc212e942f49b20a0a0fd
nguyenduchieu <hd4503810@gmail.com>
2025-04-06 12:42:18 +0700

    gs

--- lib/screens/design5.dart
+++ lib/screens/design5.dart
@@ -1,37 +1,86 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 
-class MyJsonPage extends StatelessWidget {
+class MyJsonPage extends StatefulWidget {
+  @override
+  _MyJsonPageState createState() => _MyJsonPageState();
+}
+
+class _MyJsonPageState extends State<MyJsonPage> {
+  // Dữ liệu JSON
   final String jsonString = '''[
-    {
-      "id": 1,
-      "name": "Bánh mì Sourdough",
-      "link": "https://your-image-link.com/bread1.jpg",
-      "isRead": true
-    },
-    {
-      "id": 2,
-      "name": "Bánh mì nguyên cám",
-      "link": "https://your-image-link.com/bread2.jpg",
-      "isRead": false
-    }
+  {
+    "id": 1,
+    "name": "Alex Nelson",
+    "avatar": "https://m.media-amazon.com/images/S/pv-target-images/869079f492800cac2a3a310b9f1453caf0bb46a6ea6906cc0b9f8f0e6d3c0727._SX1080_FMjpg_.jpg",
+    "isRead": true
+  },
+  {
+    "id": 2,
+    "name": "Peter Carlsson",
+    "avatar": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Cristiano_Ronaldo%2C_2023.jpg/250px-Cristiano_Ronaldo%2C_2023.jpg",
+    "isRead": true
+  },
+  {
+    "id": 3,
+    "name": "Jane Doe",
+    "avatar": "https://laban.edu.vn/wp-content/uploads/2024/02/11.2_Emma_Watson.jpg",
+    "isRead": false
+  },
+  {
+    "id": 4,
+    "name": "David Beckham",
+    "avatar": "https://images2.thanhnien.vn/528068263637045248/2024/3/3/david-beckham--1709462031579143310555.png",
+    "isRead": false
+  },
+  {
+    "id": 5,
+    "name": "David Beckham",
+    "avatar": "https://upload.wikimedia.org/wikipedia/commons/1/15/Andrea_Pirlo%2C_Save_the_Dream_at_the_Supercoppa_%28cropped%29.jpg",
+    "isRead": false
+  },
+  {
+    "id": 6,
+    "name": "David Beckham",
+    "avatar": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2012/03/26/11/Del-Piero.jpg",
+    "isRead": false
+  }
   ]'''; // Giả sử bạn đã có jsonString
+
+  // Biến lưu trữ dữ liệu JSON đã giải mã
+  List<Map<String, dynamic>> jsonList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Giải mã JSON vào trong danh sách
+    jsonList = List<Map<String, dynamic>>.from(jsonDecode(jsonString));
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> jsonList = List<Map<String, dynamic>>.from(jsonDecode(jsonString));
-
     return Scaffold(
-      appBar: AppBar(title: Text('Danh sách Bánh Mì')),
+      appBar: AppBar(
+        title: Text('New chat'),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            print('Thoat');
+          },
+        ),
+      ),
       body: ListView.builder(
         itemCount: jsonList.length,
         itemBuilder: (context, index) {
           var item = jsonList[index];
+
           return ListTile(
             leading: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
+              borderRadius: BorderRadius.circular(800),
               child: Image.network(
-                item['link'],
+                item['avatar'],
                 width: 50,
                 height: 50,
                 fit: BoxFit.cover,
@@ -40,9 +89,17 @@ class MyJsonPage extends StatelessWidget {
             title: Text(item['name']),
             subtitle: Text('ID: ${item['id']}'),
             trailing: Icon(
-              item['isRead'] ? Icons.check_circle : Icons.radio_button_unchecked,
-              color: item['isRead'] ? Colors.green : Colors.red,
+              Icons.message,
+              color: item['isRead']
+                  ? Colors.green
+                  : const Color.fromARGB(255, 119, 119, 119),
             ),
+            onTap: () {
+              setState(() {
+                // Cập nhật lại giá trị isRead và làm mới danh sách
+                item['isRead'] = !item['isRead'];
+              });
+            },
           );
         },
       ),
