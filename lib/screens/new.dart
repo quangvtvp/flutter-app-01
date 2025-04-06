2a1c6c5d1ee7fe0c38b035fb0b92d0f10ad92af3
Hoang5357 <ht4727430@gmail.com>
2025-04-06 11:02:42 +0700

    todolist

--- lib/screens/new.dart
+++ lib/screens/new.dart
@@ -1,200 +1,98 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-class RowAndColumn extends StatelessWidget {
-  const RowAndColumn({super.key});
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<Map<String, dynamic>> tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadTodoList();
+  }
+
+  Future<void> loadTodoList() async {
+    final String response = await rootBundle.loadString('assets/todo.json');
+    final List<dynamic> data = jsonDecode(response);
+    setState(() {
+      tasks = data.map((item) => Map<String, dynamic>.from(item)).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Row and Column'),
+        title: Center(child: Text('Todo list')),
+        backgroundColor: Colors.purple,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
       ),
-      body: Column(
-        children: [
-          _buildRow1(),
-          const Divider(
-            color: Colors.black,
-            height: 1,
-            thickness: 1,
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Thursday, 12 October',
+            style: TextStyle(
+              color: Colors.purple,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-          _buildRow2(),
-          const Divider(
-            color: Colors.black,
-            height: 1,
-            thickness: 1,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: tasks.length,
+            itemBuilder: (context, index) {
+              final task = tasks[index];
+              return CheckboxListTile(
+                activeColor: Colors.purple,
+                title: Text(
+                  task['title'],
+                  style: TextStyle(
+                    decoration: task['completed']
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                  ),
+                ),
+                value: task['completed'],
+                onChanged: (bool? value) {
+                  setState(() {
+                    tasks[index]['completed'] = value!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              );
+            },
           ),
-          _buildRow3(),
-          const Divider(
-            color: Colors.black,
-            height: 1,
-            thickness: 1,
-          ),
-          _buildRow4(),
-          const Divider(
-            color: Colors.black,
-            height: 1,
-            thickness: 1,
-          ),
-          _buildRow5(),
-          const Divider(
-            color: Colors.black,
-            height: 1,
-            thickness: 1,
-          ),
-          _buildRow6(),
-          const Divider(
-            color: Colors.black,
-            height: 1,
-            thickness: 1,
-          ),
-          _buildRow7(),
-          const Divider(
-            color: Colors.black,
-            height: 1,
-            thickness: 1,
-          ),
-        ],
+        ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.purple,
+        child: Icon(Icons.add),
       ),
-    );
-  }
-
-  Row _buildRow7() {
-return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        const SizedBox(
-          height: 100
-          ,
-        ),
-        Container(
-          color: Colors.red,
-          width:50,
-          height:50,
-        ),
-        Container(
-          color: Colors.green,
-          width:50,
-          height:50,
-        ),
-      ],
-    );
-  }
-  Row _buildRow6() {
-return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      
-      children: [
-        
-        
-        Container(
-          color: Colors.red,
-          width:50,
-          height:50,
-        ),
-        Container(
-          color: Colors.green,
-          width:50,
-          height:50,
-        ),
-      ],
-    );
-  }
-  Row _buildRow5() {
-return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      
-      children: [
-        Container(
-          color: Colors.red,
-          width:50,
-          height:50,
-        ),
-        Container(
-          color: Colors.green,
-          width:50,
-          height:50,
-        ),
-      ],
-    );
-  }
-  Row _buildRow4() {
-return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      
-      children: [
-       
-        Container(
-          color: Colors.green,
-          width:50,
-          height:50,
-        ),
-      ],
-    );
-  }
-  Row _buildRow3() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-     
-      children: [
-       
-        Expanded(
-          flex: 9,
-          child: Container(
-            color: Colors.red,
-            width:50,
-            height:50,
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.green,
-            width:50,
-            height:50,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Row _buildRow2() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Container(
-            color: Colors.red,
-            width:50,
-            height:50,
-          ),
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.green,
-            width:50,
-            height:50,
-          ),
-        ),
-        
-      ],
-    );
-  }
-
-  Row _buildRow1() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-      Container(
-        color: Colors.red,
-        width:50,
-        height:50,
-      ),
-        Container(
-          color: Colors.green,
-          width:50,
-          height:50,
-        ),
-      ],
     );
   }
 }
